8bc6b89d21d2e155919a605fe1bfb5b16d954ae6
YonatanRA <yeho@mail.com>
2019-12-30 21:01:07 -0600

    pair stocks

--- Pair stock app/v1/main.py
+++ Pair stock app/v1/main.py
@@ -1,22 +1,15 @@
 # librerias
-from flask import Flask 
+from flask import Flask
 from flask import render_template
-from flask import flash 
 from flask import request
-from flask import jsonify
-from flask import Markup
+
 
 
 import matplotlib
 matplotlib.use('agg')
 
-import io
-import base64
 import os
 import pandas as pd
-import numpy as np
-
-
 
 
 
@@ -26,6 +19,7 @@ DIAS_ATRAS=90
 INDICE=['^DJI']
 STOCKS=['BA','GS','UNH','MMM','HD','AAPL','MCD','IBM','CAT','TRV']
 PATH=os.path.dirname(os.path.abspath(__file__))
+
 
 # variables globales
 stock_data=None
@@ -38,7 +32,7 @@ app = Flask(__name__)
 # devuelve un dataframe
 def data():
     stock_data=[]
-    
+
     for e in INDICE+STOCKS:
         src=os.path.join(PATH, 'data/'+ e +'.csv')
         df=pd.read_csv(src)
@@ -62,43 +56,43 @@ def startup():
     global stock_data
     stock_data=data()
 
-    
-    
+
+
 
 # funcion para microservicio
 # todo, salvo el html, esta aqui
 @app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method=='POST':   # para html, subida
-        cartera=request.form['cartera']  
+        cartera=request.form['cartera']
         if cartera=='':  cartera=10000
 
         # calcula el spread
         stock1='^DJI'
         distancias={}
-        
+
         serie1=stock_data[stock1].pct_change().cumsum()
-        
+
         for stock2 in list(stock_data):
             if (stock1!=stock2):
                 serie2=stock_data[stock2].pct_change().cumsum()
                 diff=list(serie2-serie1)
                 distancias[stock2]=diff[-1]
-                
-            
+
+
         # stock mas debil y stock mas fuerte
         debil=min(distancias.items(), key=lambda x: x[1])
         fuerte=max(distancias.items(), key=lambda x: x[1])
-        
-        
+
+
         # sentido del trade
         corto=fuerte[0]
         corto_ult=stock_data[fuerte[0]][-1]
-        
+
         largo=debil[0]
         largo_ult=stock_data[debil[0]][-1]
-        
-        
+
+
         # se renderiza el html
         return render_template('index.html',
                                corto=corto,
@@ -108,12 +102,12 @@ def main():
                                largo_ult=round(largo_ult,2),
                                size_largo=round((float(cartera)*.5)/largo_ult,2),
                                cartera=cartera)
-    
-    
-    
+
+
+
     else:   # parametros por defecto
-        
-        
+
+
         return render_template('index.html',
                                corto='Nada',
                                largo='Nada',
@@ -122,17 +116,14 @@ def main():
                                largo_ult=0,
                                size_largo=0,
                                cartera=CARTERA)
-    
-     
 
-    
-# ejecucion 
-if __name__=='__main__':
-    app.run(debug=True)
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
